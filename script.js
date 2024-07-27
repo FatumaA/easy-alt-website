b54f6f843e89e3a39ffd1e2873d4b59598fa7a2e
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-07-28 01:35:21 +0300

    feat: implement dark mode 😎

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const htmlElement = document.documentElement;
+const themeSwitch = document.getElementById("theme-switch");
+const themeName = document.getElementById("theme-name");
 
+themeSwitch.addEventListener("change", function () {
+	if (this.checked) {
+		htmlElement.setAttribute("data-theme", "light");
+		themeName.innerText = "Light";
+	} else {
+		htmlElement.setAttribute("data-theme", "dark");
+		themeName.innerText = "Dark";
+	}
+});
