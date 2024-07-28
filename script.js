6908af9b1f8ee1884ffb8c6945b4efec150cf30d
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-07-28 03:24:07 +0300

    fix: get both theme switchers working

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const htmlElement = document.documentElement;
 const themeSwitch = document.getElementById("theme-switch");
+const themeSwitchMobile = document.getElementById("theme-switch-mobile");
 const themeName = document.getElementById("theme-name");
 const menuIcon = document.querySelector(".menu-icon");
 const navLinks = document.querySelector(".nav-links");
 
 themeSwitch.addEventListener("change", function () {
+	if (this.checked) {
+		htmlElement.setAttribute("data-theme", "light");
+		themeName.innerText = "Light";
+	} else {
+		htmlElement.setAttribute("data-theme", "dark");
+		themeName.innerText = "Dark";
+	}
+});
+
+themeSwitchMobile.addEventListener("change", function () {
 	if (this.checked) {
 		htmlElement.setAttribute("data-theme", "light");
 		themeName.innerText = "Light";
